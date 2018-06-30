4d265817374c354bb51905fd28b8ba6a7131aeb4
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-07-01 00:19:01 +0700

    1

--- base/docker-entrypoint.sh
+++ base/docker-entrypoint.sh
@@ -2,6 +2,34 @@
 
 set -e
 
+# option with entrypoint
+if [ -f "/option.sh" ]; then /option.sh; fi
+
+umask 0002
+
+declare -a es_opts
+
+while IFS='=' read -r envvar_key envvar_value
+do
+    # Elasticsearch env vars need to have at least two dot separated lowercase words, e.g. `cluster.name`
+    if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ ]]; then
+        if [[ ! -z $envvar_value ]]; then
+          es_opt="-E${envvar_key}=${envvar_value}"
+          es_opts+=("${es_opt}")
+        fi
+    fi
+done < <(env)
+
+export ES_JAVA_OPTS="-Des.cgroups.hierarchy.override=/ $ES_JAVA_OPTS"
+
+# Determine if x-pack is enabled
+if bin/elasticsearch-plugin list -s | grep -q x-pack; then
+    if [[ -n "$ELASTIC_PASSWORD" ]]; then
+        [[ -f config/elasticsearch.keystore ]] ||  bin/elasticsearch-keystore create
+        echo "$ELASTIC_PASSWORD" | bin/elasticsearch-keystore add -x 'bootstrap.password'
+    fi
+fi
+
 # Add elasticsearch as command if needed
 if [ "${1:0:1}" = '-' ]; then
 	set -- elasticsearch "$@"
@@ -11,18 +39,9 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of user-mutable directories to elasticsearch
-	for path in \
-		/usr/share/elasticsearch/data \
-		/usr/share/elasticsearch/logs \
-	; do
-		chown -R elasticsearch:elasticsearch "$path"
-	done
-	
-	set -- su-exec elasticsearch "$@"
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/{data,logs}
+
+	set -- su-exec elasticsearch "$@" "${es_opts[@]}"
 fi
 
-# As argument is not related to elasticsearch,
-# then assume that user wants to run his own process,
-# for example a `bash` shell to explore this image
 exec "$@"
